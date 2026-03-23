1f9719bcac6aea64c65916620dac91db41bbe031
eyther7 <tonmoyeyther@gmail.com>
2026-03-23 15:07:46 +0600

    day 2 js

---
+++ oparetor &  conditional statment/script.js
@@ -0,0 +1,89 @@
+/*
+
+//comment on js
+
+//  for one line
+
+ for multiple line
+for multiple line
+for multiple line
+for multiple line
+
+ end*/
+
+
+
+
+/* 
+    oparetors
+
+        arithmatic oparetor
+    addition +
+    subtract -
+    divide /
+    multiply *
+    module %
+    exponentiation ** power 
+
+         unary op
+    increment op ++
+    Decrement op --
+
+        assingment op
+    =
+    +=
+    -=
+    *=
+    %=
+    **=  
+       
+         compairasion op
+    equalto ==
+    equal to & type ===
+    not equal to !=
+    not equal to & type !==
+
+    > >= < <=
+
+         Logical op
+    AND &&
+    OR ||
+    NOT !
+
+
+end*/
+
+
+/*
+Conditional statment
+
+      if statment
+ let age = 22;
+
+      if (age >= 21) {
+        console.log("not allow")
+      }
+       if (age >= 22) {
+        console.log("allow")
+      }  
+        if else statment 
+
+let num = 21;
+
+      if (num % 2 === 0) {
+        console.log("Even num")
+      } else {
+        console.log("odd")
+      }
+
+
+      
+ */
+
+      let num = 21;
+
+      if (num % 2 === 0) {
+        console.log("Even num")
+      } else {
+        console.log("odd")
+      }
