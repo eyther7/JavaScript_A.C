15faf0dee755b4d33b72f8dc24600a0267565610
eyther7 <tonmoyeyther@gmail.com>
2026-03-23 19:01:21 +0600

    day 2.1 js

--- oparetor &  conditional statment/script.js
+++ oparetor &  conditional statment/script.js
@@ -25,9 +25,15 @@ for multiple line
     module %
     exponentiation ** power 
 
+
+
+
          unary op
     increment op ++
     Decrement op --
+
+
+
 
         assingment op
     =
@@ -57,7 +63,7 @@ end*/
 /*
 Conditional statment
 
-      if statment
+      if statment*********
  let age = 22;
 
       if (age >= 21) {
@@ -66,7 +72,9 @@ Conditional statment
        if (age >= 22) {
         console.log("allow")
       }  
-        if else statment 
+
+
+        if else statment ************
 
 let num = 21;
 
@@ -76,14 +84,68 @@ let num = 21;
         console.log("odd")
       }
 
+      else if statment **************
 
+  let mode = "blue";
+
+      let color;
+
+      if (mode === "dark") {
+        color ="black";
+      }else if(mode === "blue") {
+        color ="blue"
+      }else {
+        color === white;
+      }
       
  */
 
-      let num = 21;
+      /* 
 
-      if (num % 2 === 0) {
-        console.log("Even num")
-      } else {
-        console.log("odd")
-      }
+         ternary Op
+
+         condition ? true output : false output:
+
+      */
+
+        /*
+        
+        practice
+
+
+
+        
+
+          let user = prompt("enter a number:");
+
+        if (0 === user%5 ) {
+          console.log("pass")
+        } else {
+          console.log("fail")
+        };
+
+
+
+
+
+
+
+          let num = prompt("give number:");
+
+       if (num >= 90 && num <= 100) {
+        console.log("student get A");
+       }else if (num >= 70 && num <= 89) {
+        console.log("student get B")
+       }else if(num >= 60 && num <= 69) {
+        console.log("student get C")
+       }else if(num >= 50 && num <= 59) {
+        console.log("student get D")
+       }else{
+        console.log("FAIL")
+       };
+
+
+        */
+
+
+       
