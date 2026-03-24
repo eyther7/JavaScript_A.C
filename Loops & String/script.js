0cf5276b395216797a5872483d349a987f6f731b
eyther7 <tonmoyeyther@gmail.com>
2026-03-24 16:31:17 +0600

    js

---
+++ Loops & String/script.js
@@ -0,0 +1,41 @@
+/*
+
+Loops
+
+
+For Loops ****************************
+
+for (let i = 1; i <= 5; i++) {
+console
+}
+
+
+
+for (let c = 1; c <= 100; c++) {
+    console.log("Eyther") // print 6 times
+}
+
+
+
+
+
+let sum  = 0;
+
+for (let i = 1; i <= 10; i++) {
+    sum = sum+i;
+}
+console.log("sum =", sum) //  sum = 55
+
+
+for (let i = 1; i <= 20; i++) {
+    console.log("i =", i) // print 1 - 20 all number
+}
+
+
+
+
+
+while loops *******************************
+*/
+
+//23:21 vid
